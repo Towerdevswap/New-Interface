0982cd291d68e375bea71a06a461ccfcded7e2ff
towerdevswap <akmalhanif672@gmail.com>
2023-06-23 03:07:26 +0700

    new commit

---
+++ apps/web/src/config/constants/contracts.ts
@@ -0,0 +1,203 @@
+import { multicallAddresses } from '@pancakeswap/multicall'
+
+// Masterchef v1: 0xbeF35383ce0a377D618f309CCfCA7A702850817d
+
+export default {
+  masterChef: {
+    8081: '',
+    7001: '0x86AF084E7d9Ca3D7a435F69295cDFB34416A7eBd',
+    256256: '0xd964CB5A5cE6B7c8fA26796760F8A5AeE9bee1a1',
+  },
+  masterChefV1: {
+    8081: '',
+    7001: '0x92ebcdA1e2C2d5B388d3f5450cF46930F0D179A0',
+    256256: '0x60229Cdb2647803c42D9E37E71cDCAB489f6e859',
+  },
+  sousChef: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  lotteryV2: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  multiCall: multicallAddresses,
+  pancakeProfile: {
+    8081: '',
+    7001: '0x843887ac63f97dA742d35C7e150069ff5827Cf80',
+    256256: '',
+  },
+  pancakeBunnies: {
+    8081: '',
+    7001: '0xadfa8f3558d4937603df64f286d405300d6ee32c',
+    256256: '',
+  },
+  bunnyFactory: {
+    8081: '',
+    7001: '0x3997b0215D24984A7500645D2aD37ee2417B9e07',
+    256256: '',
+  },
+  claimRefund: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  pointCenterIfo: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  bunnySpecial: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  tradingCompetitionEaster: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  tradingCompetitionFanToken: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  tradingCompetitionMobox: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  tradingCompetitionMoD: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  easterNft: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  cakeVault: {
+    8081: '',
+    256256: '0x72f78a9B330cBA12F046BBf3C7D18610061fC65c',
+    7001: '',
+  },
+  cakeFlexibleSideVault: {
+    8081: '',
+    256256: '0xD962DAE1d0DCF72dEfA8ad4dd37D34837ad61b2d',
+    7001: '',
+  },
+  predictionsBNB: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  predictionsCAKE: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  chainlinkOracleBNB: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  chainlinkOracleCAKE: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  predictionsV1: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  bunnySpecialCakeVault: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  bunnySpecialPrediction: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  bunnySpecialLottery: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  bunnySpecialXmas: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  farmAuction: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  AnniversaryAchievement: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  nftMarket: {
+    8081: '',
+    7001: '0xf5d0451128064c1aa5b3dff43fa2608d97c2572d',
+    256256: '',
+  },
+  nftSale: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  pancakeSquad: {
+    8081: '',
+    7001: '0xEdFa035C3db4709ebcFDd88E722B012fcCDd2139',
+    256256: '',
+  },
+  potteryDraw: {
+    8081: '',
+    7001: '0x4203646a8C1e452C54AC59ee34Bb1D101607Cac5',
+    256256: '',
+  },
+  // TODO: multi
+  zap: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  iCake: {
+    8081: '',
+    7001: '0x04f7462b2C5418aa93d17CB72921C82560415598',
+    256256: '',
+  },
+  bCakeFarmBooster: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  bCakeFarmBoosterProxyFactory: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  nonBscVault: {
+    8081: '',
+    7001: '0x4EFe0F3A0a4d037b2770Db655C8c9708B15aeC88',
+    256256: '',
+  },
+  crossFarmingSender: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+  crossFarmingReceiver: {
+    8081: '',
+    7001: '',
+    256256: '',
+  },
+}
---
+++ packages/uikit/src/components/Svg/Icons/Faucet.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Svg from "../Svg";
+import { SvgProps } from "../types";
+
+const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
+  return (
+    <Svg viewBox="0 0 24 24" {...props}>
+    <image
+      width="24"
+      height="24"
+      href="https://i.ibb.co/KsC5FGk/faucet.png"
+    />
+    </Svg>
+  );
+};
+
+export default Icon;
